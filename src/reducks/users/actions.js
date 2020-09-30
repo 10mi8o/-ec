094791b28eadb7780791e65c8489a8e8a6842cb3
10mi8o <totoma855@gmail.com>
2020-09-30 10:10:55 +0900

    SIGNOUTのpayloadを定義

--- src/reducks/users/actions.js
+++ src/reducks/users/actions.js
@@ -14,5 +14,10 @@ export const SIGN_OUT = "SIGN_OUT";
 export const signOutAction = () => {
   return {
     type: "SIGN_OUT",
+    payload: {
+      isSignedIn: false,
+      uid: "",
+      username: ""
+    }
   }
 }
